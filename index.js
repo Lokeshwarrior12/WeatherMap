3ad2db0760ca91a376d61b2a1223e9437d18d760
Lokeshwar Kodipunjula <110045286+Lokeshwarrior12@users.noreply.github.com>
2025-02-07 06:26:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,8 +24,10 @@ app.use(cors());
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log("✅ MongoDB Connected"))
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000 // Increase timeout to 30 seconds
+  })
+  .then(() => console.log("✅ MongoDB Connected"))
   .catch(err => console.error("❌ MongoDB Connection Error:", err));
 
 // Define Schema & Model
